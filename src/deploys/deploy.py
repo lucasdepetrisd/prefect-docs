8b5dc7d0fe780f91f5e6756436f5ec05047752c7
lucasdepetrisd <lucasdepetris14@gmail.com>
2024-01-12 19:08:58 -0300

    Agrego deploy.py para testear pools y deploys

---
+++ src/deploys/deploy.py
@@ -0,0 +1,24 @@
+from prefect import flow, task
+
+from electracommons.log_config import PrefectLogger, obtener_path_script
+
+logger_prefect = PrefectLogger(__file__)
+
+@task
+def mi_tarea():
+    logger = logger_prefect.obtener_logger_prefect()
+    logger.info("Iniciando tarea por prefect...")
+    logger.info("Tarea finalizada por prefect...")
+
+@flow
+def my_flow():
+    logger = logger_prefect.obtener_logger_prefect()
+    logger.info("Hola pero de prefect")
+    logger.info("Script path: %s", obtener_path_script(__file__))
+    mi_tarea()
+
+
+if __name__ == '__main__':
+    # my_flow.serve(name="my-second-deployment",
+    #                tags=["testing"])
+    my_flow()
